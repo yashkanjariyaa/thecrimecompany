4bdd3430cf8874a0e5dc2e5b7382e0d003873204
yashkanjariyaa <yashkanjariyaff@gmail.com>
2024-07-03 15:28:56 +0530

    production changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,39 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 import connect from "./database/connection";
 import { PORT } from "./config";
 import router from "./routes/routes";
 
 const app = express();
+
+//middlewares
 app.use(express.json());
+app.use(morgan("combined"));
+app.use(helmet());
+app.use(cors());
+
 connect();
 
-app.get("/", router);
+app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send({ message: "Something went wrong!" });
+});
+
+app.use(express.static("public"));
+
+// Start server
+const HOST = process.env.HOST || "localhost";
+const server = app.listen(PORT, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
+});
+
+//graceful shutdown
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
 });
